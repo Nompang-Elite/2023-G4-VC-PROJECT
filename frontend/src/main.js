434826e0f8ac5baa01fda3f82be8bc56e16460fa
ShitaroKhido <khidpagna01@gmail.com>
2023-07-10 16:36:50 +0700

    [Initial]

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,11 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import { createPinia } from "pinia";
-import axios from "axios";
 
 loadFonts();
 
 createApp(App)
   .use(createPinia())
-  .use(axios)
   .use(router)
   .use(vuetify)
   .mount("#app");
