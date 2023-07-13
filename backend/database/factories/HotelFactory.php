136b89a2ce80755367b8e5ece50a3df1ad393cc7
ShitaroKhido <khidpagna01@gmail.com>
2023-07-13 14:31:38 +0700

    [initial]: added seeder for hotel table

---
+++ backend/database/factories/HotelFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class HotelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => $this->faker->name(),
+            "image" => "https://picsum.photos/200/300",
+            "location" => $this->faker->text(100),
+            "description" => $this->faker->text(),
+        ];
+    }
+}
