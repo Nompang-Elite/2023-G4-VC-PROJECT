f860182d7ff905c7924e2b8dac2bd903a17597d9
lekphin <phinlek20@gamil.com>
2023-07-13 10:07:27 +0700

    [BVC-87]: Created search hotel name method

--- backend/app/Http/Controllers/Api/HotelController.php
+++ backend/app/Http/Controllers/Api/HotelController.php
@@ -5,16 +5,40 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ShowhotelResource;
 use App\Models\Hotel;
+use App\Traits\HttpResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HotelController extends Controller
 {
-// ---------------get all hotels---------------
+    use HttpResponse;
+
+    // ---------------get all hotels---------------
     public function index()
     {
         $hotel = Hotel::all();
         $hotel = ShowhotelResource::collection($hotel);
-      
-        return response()->json(['success' => true, 'data' =>$hotel],200);
+
+        return response()->json(['success' => true, 'data' => $hotel], 200);
     }
-}
+
+
+    /**
+     * 
+     * Search function
+     * 
+     * **/
+    public function search(Request $req)
+    {
+        $input = $req->query('s');
+        // Search Query
+        $hotels =  DB::select('select * from hotels where hotels.name like :input', ["input" => "%" . $input . "%"]);
+        // Return Data
+        if (!count($hotels) <= 0) {
+            // Return the result data
+            return $this->success($hotels, "search result", 200);
+        }
+        // Error if no result
+        return $this->error("not match!");
+    }
+};
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\HotelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,5 @@ Route::group(['prefix' => '/v1/auth'], function () {
     // More here...
 });
 
-Route::get('/hotel', [HotelController::class,'index']);
+Route::get('/hotel', [HotelController::class, 'index']);
+Route::get('/hotel/search', [HotelController::class, 'search']);
