e4500e78250d1bd09b54e73bffed491d70aabda5
ShitaroKhido <khidpagna01@gmail.com>
2023-07-13 14:42:52 +0700

    [initial]: Fixed error and update response on hotel controller.

--- backend/app/Http/Controllers/Api/HotelController.php
+++ backend/app/Http/Controllers/Api/HotelController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\ShowhotelResource;
+use App\Http\Resources\ShowHotelResource;
 use App\Models\Hotel;
+use App\Traits\HttpResponse;
 use Illuminate\Http\Request;
 
 class HotelController extends Controller
 {
-// ---------------get all hotels---------------
+    use HttpResponse;
+    // ---------------get all hotels---------------
     public function index()
     {
         $hotel = Hotel::all();
-        $hotel = ShowhotelResource::collection($hotel);
-      
-        return response()->json(['success' => true, 'data' =>$hotel],200);
+        $hotel = ShowHotelResource::collection($hotel);
+        if ($hotel)
+            return $this->success($hotel, "list of hotels");
+
+        return $this->error("list is empty", 404);
     }
 }
