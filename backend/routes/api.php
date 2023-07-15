28512b5d05bb39aee02f966e5e58d37a4a918f0d
ShitaroKhido <khidpagna01@gmail.com>
2023-07-15 23:01:54 +0700

    [error]: error log, restoring file and code.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,19 +2,19 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\HotelController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\RoomController;
 use Illuminate\Support\Facades\Route;
 
 
 
 
 //Hotel controller route
-Route::get('/hotel', [HotelController::class,'index']);
-Route::get('/hotel/{id}', [HotelController::class,'show']);
-Route::delete('/hotel/{id}', [HotelController::class,'destroy']);
+Route::get('/hotel', [HotelController::class, 'index']);
+Route::get('/hotel/{id}', [HotelController::class, 'show']);
+Route::delete('/hotel/{id}', [HotelController::class, 'destroy']);
 //room controller route
-Route::get('/rooms', [RoomController::class,'index']);
-Route::get('/hotel/{id}', [RoomController::class,'show']);
+Route::get('/rooms', [RoomController::class, 'index']);
+Route::get('/hotel/{id}', [RoomController::class, 'show']);
 
 Route::get('/hotel', [HotelController::class, 'index']);
 Route::get('/hotel/search', [HotelController::class, 'search']);
