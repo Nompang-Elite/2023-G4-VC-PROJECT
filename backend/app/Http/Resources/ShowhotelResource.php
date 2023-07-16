739714cb6832ee7a02dc78461f1f092e370c5fe7
SREYTOUCH.THOR <THOR.SREYTOUCH@PNC.LAN>
2023-07-16 13:33:38 +0700

    [coflict]: error log

--- backend/app/Http/Resources/ShowhotelResource.php
+++ backend/app/Http/Resources/ShowhotelResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ShowhotelResource extends JsonResource
+class ShowHotelResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
