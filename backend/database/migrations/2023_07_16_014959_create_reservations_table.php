8d47014702f3977871b6ddfb5fa226e23bc10b0f
ShitaroKhido <khidpagna01@gmail.com>
2023-07-16 09:08:52 +0700

    [Initial]: Added migration on booking proess

---
+++ backend/database/migrations/2023_07_16_014959_create_reservations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->date("date_in");
+            $table->date("date_out");
+            $table->string("made_by", 40)->default("unknown");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
