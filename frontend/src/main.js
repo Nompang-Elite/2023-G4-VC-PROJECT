1025fda093befc91e017cb90be2f837e2accb609
hayya <remhay07@gmial.com>
2023-07-17 07:43:07 +0700

    [BVC-10]: Import date picker into Booking view

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,8 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import routes from "./routes";
 import { createPinia } from "pinia";
+import VueDatePicker from '@vuepic/vue-datepicker';
 
 loadFonts();
 
-createApp(App).use(createPinia()).use(routes).use(vuetify).mount("#app");
+createApp(App).use(createPinia()).use(routes).use(vuetify).component('VueDatePicker', VueDatePicker).mount("#app");
