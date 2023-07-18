de851c832d6f0af9f092f30d5ad4328a961249a5
ShitaroKhido <khidpagna01@gmail.com>
2023-07-18 18:52:40 +0700

    [BVC-54]: Updated router and route approach.

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -14,32 +14,62 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("@/views/HomeView.vue"),
+    children: [
+      {
+        path: "",
+        component: () => import("@/views/HomeView.vue"),
+      },
+      {
+        /**
+         * Route to the hotel views
+         * */
+        path: "hotels",
+        name: "Hotels",
+        component: () => import("@/views/HotelsView.vue"),
+      },
+      {
+        /**
+         * Route to the near me views
+         * */
+        path: "near_me",
+        name: "NearMe",
+        component: () => import("@/views/NearMe.vue"),
+      },
+      {
+        /**
+         * Route to the abouts views
+         * */
+        path: "abouts",
+        name: "Abouts",
+        component: () => import("@/views/HomeView.vue"),
+      },
+      {
+        path: "search",
+        name: "Search",
+        children: [
+          {
+            /**
+             * This will render the parent route
+             * **/
+            path: "",
+            name: "Result",
+            component: () => import("@/views/SearchView.vue"),
+          },
+          {
+            /**
+             * This will be rendered when the search result is not found
+             * */
+            path: "not_found",
+            name: "Not Found",
+            component: () => import("@/components/Errors/NotFound.vue"),
+          },
+        ],
+        meta: { isAdmin: false, isGuest: true },
+      },
+    ],
+    meta: { isAdmin: false, isGuest: true },
   },
-  {
-    /**
-     * Route to the hotel views
-     * */
-    path: "/hotels",
-    name: "Hotels",
-    component: () => import("@/views/HotelsView.vue"),
-  },
-  {
-    /**
-     * Route to the near me views
-     * */
-    path: "/near_me",
-    name: "NearMe",
-    component: () => import("@/views/NearMe.vue"),
-  },
-  {
-    /**
-     * Route to the abouts views
-     * */
-    path: "/abouts",
-    name: "Abouts",
-    component: () => import("@/views/HomeView.vue"),
-  },
+
   {
     //Solution Redirection: https://stackoverflow.com/questions/50961082/vue-js-how-to-redirect-to-a-common-route-if-route-isnt-found
     // Vue 3 need to use /:pathMatch(.*)*
@@ -47,27 +77,24 @@ const routes = [
     name: "Not Found",
     component: () => import("@/components/Errors/NotFound.vue"),
   },
+
   {
-    path: "/search",
-    name: "Search",
+    path: "/admin",
     children: [
       {
-        /**
-         * This will render the parent route
-         * **/
         path: "",
-        name: "Result",
-        component: () => import("@/views/SearchView.vue"),
+        // component: () => import("@/views/Admin/HomeAdmin.vue"),
       },
       {
-        /**
-         * This will be rendered when the search result is not found
-         * */
-        path: "not_found",
-        name: "Not Found",
-        component: () => import("@/components/Errors/NotFound.vue"),
+        path: "users",
+        // component: () => import("@/views/Admin/HomeAdmin.vue"),
+      },
+      {
+        path: "hotels",
+        // component: () => import("@/views/Admin/HomeAdmin.vue"),
       },
     ],
+    meta: { isAdmin: true, requiredLoggedIn: true },
   },
 
   // More route goes here!!!
@@ -82,7 +109,11 @@ const routes = [
  *
  * */
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+// Route Gaurd goes here
+
+export default router;
