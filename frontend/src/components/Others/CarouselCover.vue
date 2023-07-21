4c9893926f647832256558218b71c1f94cdf7d98
ShitaroKhido <khidpagna01@gmail.com>
2023-07-21 16:31:00 +0700

    [hotfix]: added fake image link corousel card

--- frontend/src/components/Others/CarouselCover.vue
+++ frontend/src/components/Others/CarouselCover.vue
@@ -2,14 +2,12 @@
   <v-carousel height="400" hide-delimiters cycle interval="3500">
     <!-- Carousel Item -->
     <v-carousel-item v-for="(hotel, i) in slides" :key="i">
-      <v-sheet>
         <v-img
           aspect-ratio="16/9"
           cover
-          src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+          :src="hotel.image"
         >
         </v-img>
-      </v-sheet>
     </v-carousel-item>
   </v-carousel>
 </template>
