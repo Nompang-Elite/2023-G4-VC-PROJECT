06993938aeb1317a7988eb6bed5deb2c9ac23a92
ShitaroKhido <khidpagna01@gmail.com>
2023-07-27 15:52:58 +0700

    [hotfix]: image uploading added to register hotel.

--- frontend/src/store/AdminStore.js
+++ frontend/src/store/AdminStore.js
@@ -24,9 +24,9 @@ export const useAdminStore = defineStore("Admin", {
         postal_code: null,
         hotel_phone: null,
         hotel_email: null,
-        hotel_imgs: null,
       },
       errors: [],
+      hotel_imgs: [],
     };
   },
   actions: {
@@ -39,36 +39,54 @@ export const useAdminStore = defineStore("Admin", {
       // console.log(form);
       api.api_base
         .post("/admin/hotels/register", form)
-        .then(() => {
-          this.Admin.registerHotelForm = {
-            firstname: null,
-            lastname: null,
-            email: null,
-            phone: null,
-            gender: null,
-            password: null,
-            password_confirmation: null,
-            hotel_name: null,
-            description: null,
-            city: null,
-            country: null,
-            address: null,
-            postal_code: null,
-            hotel_phone: null,
-            hotel_email: null,
-            hotel_imgs: [],
-          };
-          this.Admin.registerHotelDialog = false;
+        .then((res) => {
+          this.uploadHotelImg(res.data.data.hotel.id);
         })
         .catch((error) => {
           if (error.response) {
             console.log("Status:", error.response.status);
             console.log("Data:", error.response.data.errors);
-            alert("Register failed please try again!");
           } else {
             console.log("Error:", error.message);
           }
         });
+    },
+    uploadHotelImg(id) {
+      this.hotel_imgs.forEach((file) => {
+        api.api_base
+          .post(
+            "/hotel/upload/image",
+            { hotel_id: id, image: file },
+            //Solution for multi form data:
+            //  https://stackabuse.com/axios-multipart-form-data-sending-file-through-a-form-with-javascript/
+            { headers: { "Content-Type": "multipart/form-data" } }
+          )
+          .then(() => {
+            this.registerHotelDialog = false;
+            this.uploadDialog = false;
+            this.hotel_imgs = [];
+            this.registerHotelForm = {
+              firstname: null,
+              lastname: null,
+              email: null,
+              phone: null,
+              gender: null,
+              password: null,
+              password_confirmation: null,
+              hotel_name: null,
+              description: null,
+              city: null,
+              country: null,
+              address: null,
+              postal_code: null,
+              hotel_phone: null,
+              hotel_email: null,
+            };
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     },
   },
 });
